1b0159b0377c7b5d9990af462009dab41829274d
unknown <guillermoaprile7@gmail.com>
2022-09-19 12:30:06 -0300

    Creacion del Proyecto. Guillermo Aprile

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './index.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <h1>HOLIS</h1>
+    </>
+  );
+}
+
+export default App;
